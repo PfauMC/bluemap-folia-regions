501c1f1d95c1a9e73b52b58e00affc658d3c0382
Lexie Malina <alex@t-ch.net>
2024-10-13 18:41:31 -0500

    Bump paperweight-userdev to "1.7.3"

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,10 +11,8 @@ pluginManagement {
 dependencyResolutionManagement {
     versionCatalogs {
         create("libs") {
-            plugin("shadow", "com.github.johnrengelman.shadow").version("8.1.1")
-            plugin("paper", "io.papermc.paperweight.userdev").version("1.7.0")
-            plugin("runpaper", "xyz.jpenilla.run-paper").version("2.3.0")
             plugin("shadow", "com.gradleup.shadow").version("8.3.3")
+            plugin("paper", "io.papermc.paperweight.userdev").version("1.7.3")
         }
     }
 }
