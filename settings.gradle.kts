770ac71caf54f050902ed2a99ee4cf3399f6aed5
Lexie Malina <alex@t-ch.net>
2024-10-13 18:41:58 -0500

    Bump run-paper to "2.3.1"

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,6 +13,7 @@ dependencyResolutionManagement {
         create("libs") {
             plugin("shadow", "com.gradleup.shadow").version("8.3.3")
             plugin("paper", "io.papermc.paperweight.userdev").version("1.7.3")
+            plugin("runpaper", "xyz.jpenilla.run-paper").version("2.3.1")
         }
     }
 }
